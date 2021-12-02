8d56f4627dcf8f76c691941566068db763ae6d89
Jacob Madsen <boogath@gmail.com>
2021-12-01 17:19:56 -0700

    first upload

---
+++ GordonImportsNewsletter/app/src/main/java/com/example/gordonimportsnewsletter/Send.java
@@ -0,0 +1,22 @@
+package com.example.gordonimportsnewsletter;
+
+import android.app.Activity;
+
+public class Send implements java.lang.Runnable{
+
+
+    @SerializedName("range")
+    var range: String,
+    @SerializedName("majorDimension")
+    var majorDimension: String,
+    @SerializedName("values")
+    var values: ArrayList<Item>
+
+    private Activity activity;
+    public Send(Activity activity) {this.activity = activity;}
+
+    @Override
+    public void run() {
+
+    }
+}
